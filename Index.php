6a5ddd220cc2564fd43aa4f558d79eccf4ca9339
Faishal Azka <azukineru@gmail.com>
2016-09-08 19:03:35 +0700

    first commit

---
+++ Index.php
@@ -0,0 +1,26 @@
+<html>
+
+<head>
+	<title>My Very Own Website</title>
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="screen"/>
+</head>
+
+<body>
+	<h1>Hello World</h1>
+
+	<div id="main_image" style="width: 600; height: 300; border:1px solid black; margin-left: 300px; margin-top: 80px; position: absolute; z-index-5;">
+
+	</div>
+
+	<div id="main_menu_container" style="width: 300; height: 400; border:1px solid black; margin-left: 850px; margin-top: 80px; padding: 5px; position: absolute; z-index: 10">
+		<div id="menu1">
+		</div>
+		<div id="menu2">
+		</div>
+		<div id="menu3">
+		</div>
+	</div>
+
+</body>
+
+</html>
